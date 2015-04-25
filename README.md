3b09fff10543d556df2e0d5f39023fc8f1c8cccd
liuzesen <124005330@qq.com>
2015-04-26 00:05:10 +0800

    添加颜色以及button部分

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # chableUI
 自定义UI
 
-## 
+## My Promise
+* I just use this characteristics in sass: @extend, %template, @mixin, @import, and variate
+* How do I name a css class? I just use '-' to separate word group, for example: button and button-group
+* How do I name a modifier of a css class? I use '--' to separate the css name and modifier name, for example: button-group--vertical, button--success
+* How do I name a css class which belong to another css class? I use '_' to separate the css name and its parent css class
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
-gulp.task('default', function() {
-	gulp.src('./src/main.scss')
-	    .pipe(sass())
-	    .pipe(gulp.dest('./dist'));
-});
+var plugins = require('gulp-load-plugins')();
 
+// create the production file
 gulp.task('product', function() {
 	gulp.src('./src/main.scss')
 	    .pipe(sass({
@@ -14,3 +9,28 @@ gulp.task('product', function() {
 	    }))
 	    .pipe(gulp.dest('./dist'));	
 });
+
+// start local server on port 3000
+gulp.task('server', function() {
+  return plugins.connect.server({
+    root: './',
+    port: 3000,
+    livereload: true,
+    fallback: 'test/ui.html'
+  });
+});
+
+// compile the scss files
+gulp.task('sass', function() {
+	gulp.src('./src/main.scss')
+		.pipe(plugins.plumber())
+	    .pipe(plugins.sass())
+	    .pipe(gulp.dest('./dist'));
+});
+
+// watch sass and js files
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('default', [ 'server', 'sass', 'watch' ]);
